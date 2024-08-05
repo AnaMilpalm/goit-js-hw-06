d959f86c3dfdc72851718bb92a94c1081239f104
Anastacy Slyusar <anastacy764@gmail.com>
2024-08-05 17:08:23 +0200

    add task 2 and task 3

---
+++ js/task-3.js
@@ -0,0 +1,23 @@
+const profile = {
+    username: "Jacob",
+    playTime: 300,
+    changeUsername(newName) {
+        this.username = newName;
+    },
+
+    updatePlayTime(hours) {
+        this.playTime = hours;
+    },
+
+    getInfo() {
+        return `${this.username} this.username ${this.playTime} active hours!`;
+    }
+
+
+};
+
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+ profile.changeUsername("Marco");
+ console.log(profile.getInfo()); // "Marco c 300 active hours!"
